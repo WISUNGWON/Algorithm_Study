347e2b42f22b6a976b15377329ba7e45f7bd61f3
WISUNGWON <tkrhkaos12@naver.com>
2020-08-02 17:29:54 +0900

    [위성원] 백준 10866 덱

--- sungwon/src/deque/Beakjoon_10866_Sungwon.java
+++ sungwon/src/deque/Beakjoon_10866_Sungwon.java
@@ -1,10 +1,64 @@
 package deque;
 
+import java.io.IOException;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Beakjoon_10866_Sungwon {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static Deque<Integer> deque = new LinkedList<>();
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String order;
+		int num;
+		
+		for (int i = 1; i <= N; i++) {
+			order = sc.next();
+			if (order.equals("push_back")) {
+				num = sc.nextInt();
+				deque.addLast(num);;
+			} else if (order.equals("push_front")) {
+				num = sc.nextInt();
+				deque.addFirst(num);
+			} else if (order.equals("pop_front")) {
+				if (deque.size() < 1) {
+					System.out.println(-1);
+				} else {
+					System.out.println(deque.pollFirst());
+				}
+			} else if (order.equals("pop_back")) {
+				if (deque.size() < 1) {
+					System.out.println(-1);
+				} else {
+					System.out.println(deque.pollLast());
+				}
+			} else if (order.equals("size")) {
+				System.out.println(deque.size());
+			} else if (order.equals("empty")) {
+				if (deque.isEmpty()) {
+					System.out.println(1);
+				} else {
+					System.out.println(0);
+				}
+			} else if (order.equals("front")) {
+				if (deque.size() < 1) {
+					System.out.println(-1);
+				} else {
+					System.out.println(deque.peekFirst());
+				}
+			} else if (order.equals("back")) {
+				if (deque.size() < 1) {
+					System.out.println(-1);
+				} else {
+					System.out.println(deque.peekLast());
+				}
+			}
+		}
+		
+		sc.close();
 	}
 
 }
