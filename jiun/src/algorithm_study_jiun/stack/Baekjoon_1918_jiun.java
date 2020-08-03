c5d4105d476fa8dd64078e9a736beaad62dbf23a
LEE JI UN <45230079+wldnswldnswl@users.noreply.github.com>
2020-08-03 12:24:30 +0900

    Update Baekjoon_1918_jiun.java
    [이지운] 백준 1918 후위표기식

--- jiun/src/algorithm_study_jiun/stack/Baekjoon_1918_jiun.java
+++ jiun/src/algorithm_study_jiun/stack/Baekjoon_1918_jiun.java
@@ -1,5 +1,35 @@
 package algorithm_study_jiun.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
 //https://www.acmicpc.net/problem/1918
 public class Baekjoon_1918_jiun {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String expression = sc.next();
+		Stack<Character> st = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < expression.length(); i++) {
+			char ch = expression.charAt(i);
+			switch (ch) {
+			case '(':
+				break;
+			case ')':
+				while(!st.isEmpty()) {
+					System.out.print(st.pop());
+				}
+				break;
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				st.add(ch);
+				break;
+			default:
+				System.out.print(ch);
+			}
+		}
+		
+	}
 }
