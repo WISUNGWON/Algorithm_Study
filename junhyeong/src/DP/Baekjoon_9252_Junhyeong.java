ec1ac608c972fb8f2cc7b72c4ed449de7ef0b054
Junhyeong <gw4621@gmail.com>
2020-08-13 23:04:43 +0900

    [정준형] 백준 9252 LCS 2

--- junhyeong/src/DP/Baekjoon_9252_Junhyeong.java
+++ junhyeong/src/DP/Baekjoon_9252_Junhyeong.java
@@ -6,22 +6,23 @@ import java.util.Scanner;
  * 
  * @author Junhyeong
  *
- * LCS 1
+ * LCS 2
  * 
- * 152ms
+ * 160ms
  * 
  */
 
 public class Baekjoon_9252_Junhyeong {
 
-	static int lcs(String s1, String s2) {
+	static void lcs(String s1, String s2) {
 
 		int n1 = s1.length(), n2 = s2.length();
 		int[][] dp = new int[n1 + 1][n2 + 1];
-		
-		for (int i = 0; i < n1; i++) {
-			for (int j = 0; j < n2; j++) {
-				if(s1.charAt(i) == s2.charAt(j)) {
+		int i, j;
+		for (i = 0; i < n1; i++) {
+			char c = s1.charAt(i);
+			for (j = 0; j < n2; j++) {
+				if(c == s2.charAt(j)) {
 					dp[i+1][j+1] = dp[i][j] + 1;
 				}
 				else {
@@ -29,7 +30,28 @@ public class Baekjoon_9252_Junhyeong {
 				}
 			}
 		}
-		return dp[n1][n2];
+		System.out.println(dp[n1][n2]);
+		if(dp[n1][n2] == 0) {
+			return;
+		}
+		char[] ch = new char[dp[n1][n2]];
+		int idx = dp[n1][n2] - 1;
+		i = n1;
+		j = n2;
+		while(idx >= 0) {
+			if(dp[i-1][j] == dp[i][j]){
+				i --;
+			}
+			else if(dp[i][j-1] == dp[i][j]) {
+				j --;
+			}
+			else if(dp[i-1][j-1] == dp[i][j] - 1) {
+				i --;
+				j --;
+				ch[idx--] = s1.charAt(i);
+			}
+		}
+		System.out.println(String.valueOf(ch));
 	}
 
 	public static void main(String[] args) {
@@ -37,7 +59,7 @@ public class Baekjoon_9252_Junhyeong {
 		String s1 = sc.nextLine();
 		String s2 = sc.nextLine();
 		sc.close();
-		System.out.println(lcs(s1, s2));
+		lcs(s1, s2);
 	}
 
 }
